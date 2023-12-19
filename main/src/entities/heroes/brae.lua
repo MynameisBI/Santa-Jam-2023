c894d49218dd64ba4502e1ef635c0c51a4243e2e
Rumis <20.21.9.12.1bibi@gmail.com>
2023-12-19 21:04:47 +0700

    Someone doesn't know how to spell artificer

--- main/src/entities/heroes/brae.lua
+++ main/src/entities/heroes/brae.lua
@@ -9,7 +9,7 @@ local Brae = Class('Brae', HeroEntity)
 function Brae:initialize(slot)
     Entity.initialize(self)
 
-    HeroEntity.initialize(self, slot, Images.heroes.brae, 'Brae', {'bigEar', 'artificier'},
+    HeroEntity.initialize(self, slot, Images.heroes.brae, 'Brae', {'bigEar', 'artificer'},
         {
             [1] = Hero.Stats(40, 30, 1.0, 300, 0, 0),
             [2] = Hero.Stats(60, 45, 1.0, 300, 0, 0),
